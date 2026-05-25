6400fc3a8d30189fe02f6b525fa35c187abb4add
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-05-25 18:12:57 +0000

    initial commit

---
+++ src/main/java/com/ebjavademo/EbJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ebjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbJavaDemoApplication.class, args);
+    }
+
+}
